f390e84db13ad80334f0aa2fb8eb82a1aacdd318
mauricioarcez <mauricioarcez23@gmail.com>
2023-06-22 22:19:55 -0300

    desafio termindo

--- desafio_s8.py
+++ desafio_s8.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import textwrap
 
 class Usuario:
 	usuarios_registrados = []
@@ -73,22 +74,31 @@ class Usuario:
   
 		def mostrar_contenido():
 			if self.online:
-				print(' _________________________________________________________ ')
-				print('|                                                         |')
-				print('|   INFOnews                          [5.Cerrar Sesion ]  |')
-				print('|_________________________________________________________|')
-				print('|   _____________________     _______________________     |')
-				print('|  | ID#1                |   | ID#                   |    |')
-				print('|  |                     |   |                       |    |')
-				print('|  |   Boca.imagen.png   |   | + [PUBLICAR ARTICULO] |    |')
-				print('|  |                     |   |                       |    |')
-				print('|  |_____________________|   |_______________________|    |')
-				print('|  .BOCA VOLVIO A PERDER     .PRESIONE 3 PARA AGREGAR     |')
-				print('|   DE LOCAL. [4]             UN ARTICULO                 |')
-				print('|                                                         |')
-				print('| PRESIONE 4 PARA COMENTAR UN ARTICULO.                   |')
-				print('| PRESIONE 5 PARA ABANDONAR INFOnews.                     |')
-				print('|_________________________________________________________|\n')
+				print(f' ______________________________________________')
+				print(f'|                                              ')
+				print(f'|   INFOnews                 [5.Cerrar Sesion] ')
+				print(f'|______________________________________________')
+				print(f'|   _________________________________________  ')
+				print(f'|  |ID-Articulo#{a1.id}                        ')
+				print(f'|  |                                           ')
+				print(f'|  |                {a1.imagen}.png            ')
+				print(f'|  |                                           ')
+				print(f'|  |_________________________________________  ')
+				print(f'|  .{a1.titulo} [4]                            ')
+				print(f'|  -{a1.fecha_publicada}                       ')
+				print(f'|   _________________________________________  ')	
+				print(f'|  |ID-Articulo#{a2.id}                        ')
+				print(f'|  |                                           ')
+				print(f'|  |               {a2.imagen}.png             ')
+				print(f'|  |                                           ')
+				print(f'|  |_________________________________________  ')
+				print(f'|  .{a2.titulo} [4]                            ')
+				print(f'|  -{a2.fecha_publicada}                       ')
+				print(f'|                                              ')
+				print(f'| PRESIONE 3 PARA AGREGAR UN ARTICULO          ')
+				print(f'| PRESIONE 4 PARA COMENTAR UN ARTICULO.        ')
+				print(f'| PRESIONE 5 PARA CERRAR SESION.               ')
+				print(f'|______________________________________________\n')
 
 		while True:
    
@@ -103,7 +113,6 @@ class Usuario:
 				email = input('Ingrese su email:\n-> ')
 
 				self.registrar(nombre_usuario,contraseña,nombre,apellido,telefono,email)
-				se_registro = True
 				username = nombre_usuario
 				password = contraseña
 				iniciar = int(input('Desea Inciar sesion? 1/2\n1->SI.\n2->NO.\n-> '))
@@ -113,52 +122,114 @@ class Usuario:
 				else:
 					print('Hasta luego!..\n')
 					menu_principal()
-					continue
 
-			elif decision == 2 and se_registro == True:
-				self.login(username,password)
-				mostrar_contenido()
-
+			elif decision == 2:
+				try:
+					self.login(username,password)
+					mostrar_contenido()
+				except Exception:
+					print('Debes registrarte para iniciar sesion en una cuenta.\n')
+					menu_principal()
+     
 			elif decision == 3:
-				publicar = input('Para publicar articulos debes ser colaborador.\nDeseas registrarte como colaborador?\n1->SI\n2->NO.\n-> ')
-				if publicar == 1:
-					u1 = Usuario()
-					u1.registrar(nombre_usuario,contraseña,nombre,apellido,telefono,email)
-					c1 = Colaborador(u1)
+				pregunta = int(input('Desea registrarse como colaborador?\n1->SI.\n2->NO.\n-> '))
+				if pregunta == 1 and self.online:
+					c1 = Colaborador(self)
 					c1.registrar()
-					titulo = input('Ingrese un titulo llamativo para su articulo:\n-> ').upper()
-					resumen = input('Ingrese un resumen del titulo:\n-> ').capitalize()
-					contenido = input('Ingrese el contenido explicativo de su articulo:\n-> ')
-					imagen = input('Ingrese la imagen que resaltara el articulo:\n-> ')
-
-					titulo = c1.publicar(titulo,resumen,contenido,imagen,self.id)
-					print(f' ___________________________________________________ ')
-					print(f'|                                                    ')
-					print(f'|   INFOnews                     [5.Cerrar Sesion ]  ')
-					print(f'|____________________________________________________')
-					print(f'|   __________________________________')
-					print(f'|  | ID#{titulo.id}')
-					print(f'|  |                                          ')
-					print(f'|  |               {titulo.imagen}.png')
-					print(f'|  |                                          ')
-					print(f'|  |___________________________________')
-					print(f'|  .{titulo.titulo}')
-					print(f'|    AUTOR: {self.username},ID#{self.id}')
-					print(f'| \"{titulo.resumen}')
-					print(f'|')
-					print(f'|    {titulo.contenido}')
-				else:
-					print('Debes registrarte como usuario primero antes de ser Colaborador.')
-					continue
 					
+					if self.es_colaborador:
+						titulo = input('Ingrese un titulo llamativo para su articulo:\n-> ').upper()
+						resumen = input('Ingrese un resumen del titulo:\n-> ').capitalize()
+						contenido = input('Ingrese el contenido explicativo de su articulo:\n-> ')
+						imagen = input('Ingrese la imagen que resaltara el articulo:\n-> ')
+			
+						c1.publicar(titulo,resumen,contenido,imagen)
+						artic = Articulo.articulos_publicados[-1]
+							
+						print(f' __________________________________________________')
+						print(f'|                                                  ')
+						print(f'|   INFOnews                    [5.Cerrar Sesion ] ')
+						print(f'|__________________________________________________')
+						print(f'|   _____________________________________________  ')
+						print(f'|  |ID-Articulo#{artic.id}                         ')
+						print(f'|  |                                               ')
+						print(f'|  |               {artic.imagen}.png              ')
+						print(f'|  |                                               ')
+						print(f'|  |_____________________________________________  ')
+						print(f'|  .{artic.titulo}                                 ')
+						print(f'|  AUTOR: {self.username}, ID#{self.id}            ')
+						print(f'|       {artic.resumen}                            ')
+						print(f'|                                                  ')
+						print(f'|    \"{artic.contenido}\"                       \n')
+				else:
+					print('Inicie sesion para probar esta funcion.\nHasta luego..')
+					menu_principal()
+     
+			elif decision == 4:
+
+				def mostrar_comentario():
+					ultimo = Comentario.lista_comentarios[-1]
+					for articulo in Articulo.articulos_publicados:
+						if articulo.id == id_art:
+							titu = articulo.titulo
+							titu_contenido = articulo.contenido
+							break
+					wrap = textwrap.wrap(titu_contenido,width=40)
+
+					print(f' ___________________________________________ ')
+					print(f'|                                            ')
+					print(f'|          {titu}                            ')
+					print(f'| \"{wrap[0]}                                ')
+					print(f'| {wrap[1]}                                  ')
+					print(f'| {wrap[2]}\"                                ')
+					print(f'|___________________________________________ ')
+					print(f'|   _______   _____________________________  ')
+					print(f'|  |  _w   | |{self.username}:               ')
+					print(f'|  | _(")_ | |  {ultimo.contenido}           ')
+					print(f'|  |/|___|\| |_____________________________  ')
+					print(f'|                         {ultimo.fecha_hora}')
+					print(f'|   ___   _________________________________  ')
+					print(f'|  | 0 | |Escribir comentario... [4]         ')
+					print(f'|  |/_\| |_________________________________\n')
+
+				pregunta2 = int(input('Desea comentar como publico[1] o como colaborador[2] ?\n1->PUBLICO.\n2->COLABORADOR.\n-> '))
+
+				if pregunta2 == 1 and self.online:		
+					p1 = Publico(self)
+					p1.registrar()
+					if self.es_publico:
+						id_art = int(input('Ingrese el ID del articulo que desea comentar.\n-> '))
+						contenido_coment = input('Escriba su comentario:\n-> ')
+						p1.comentar(id_art,contenido_coment)
+						mostrar_comentario()
+							
+				elif pregunta2 == 2 and self.online:		
+					c1 = Colaborador(self)
+					c1.registrar()
+					if self.es_colaborador:
+						id_art = int(input('Ingrese el ID del articulo que desea comentar.\n-> '))
+						contenido_coment = input('Escriba su comentario:\n-> ')
+						c1.comentar(id_art,contenido_coment)
+						mostrar_comentario()
+				else:
+					print('Inicie sesion para probar esta funcion.\nHasta luego..')
+					menu_principal()
+      
+			elif decision == 5:
+				print('Has cerrado sesion.. Ingrese cualquier numero MAYOR a 5 para terminar el programa.\nNos vemos pronto..')
+				menu_principal()
+    
+			else:
+				print('Opciones no disponibles. Gracias por visitar INFOnews.')
+				return False
 
 class Articulo():
 
     articulos_publicados = []
 
     def __init__(self, titulo, resumen, contenido, imagen, id_usuario):
-        self.titulo = titulo
-        self.resumen = resumen
+        self.titulo = titulo.upper()
+        self.resumen = resumen.capitalize()
         self.contenido = contenido
         self.imagen = imagen
         self.id_usuario = id_usuario
@@ -174,6 +245,7 @@ class Articulo():
 class Comentario():
     
     contador_id = 0
+    lista_comentarios = []
     
     def __init__(self,id_articulo,contenido,id_usuario):
         self.id_articulo = id_articulo
@@ -184,11 +256,9 @@ class Comentario():
         self.id = Comentario.contador_id
         self.fecha_hora = datetime.now()
         self.estado = 'Activo'
-    
-    def __repr__(self):
-        return f'{self.contenido}'
+        self.lista_comentarios.append(self)
         
-
+        
 class Publico(Usuario):
 
 	publico_existente = False
@@ -241,8 +311,8 @@ class Colaborador(Usuario):
 			print('Debes registrarte como publico o colaborador para comentar articulos.\n')
 
 	def publicar(self,titulo,resumen,contenido,imagen):
-		if self.es_colaborador:
-			titulo = Articulo(titulo, resumen, contenido, imagen, self.id)
+		if self.es_colaborador == True:
+			Articulo(titulo, resumen, contenido, imagen, self.id)
 			print(f'Artículo publicado con éxito.\n')
 			
 		else:
@@ -251,11 +321,16 @@ class Colaborador(Usuario):
 
 
 
-#Creacion de usuario. con solo avatar y estado.
+#Creacion de 2 usuarios. con solo avatar y estado predefinidos y sus otros atributos en None hasta que se registren.
 u1 = Usuario()
+u2 = Usuario()
 
-#Menu intuitivo a seguir los pasos para registrarse o inicar sesion.
-u1.menu()
+#Creacion de 2 Articulos definidos para comentar
+a1 = Articulo('BOCA VOLVIO A PERDER DE LOCAL','La derrota fue contra Arsenal por 3 a 0 en la Bombonera.','contenido extenso de prueba porque no se que escribir aqui, pero se supone debe ser un poco largo','imagen.boca',1)
+a2 = Articulo('Chaco for ever tiene nuevos refuerzos', 'se trata de Lionel Messi jugador que viene desde el PSG de Francia.','Contenido extenso de prueba porque no se que escribir aqui, pero se supone debe ser un poco largo','imagen.chaco',2)
+
+#Menu intuitivo a seguir los pasos para registrarse, inicar sesion, comentar, publicar.
+Usuario.menu(u1)
 
 
 
